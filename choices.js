f6415c51fffd9457044e5c527e2df7569d76c9c4
nyang3006 <nyang3006@baysidehighschool.org>
2019-03-14 10:49:52 -0400

    Update choices.js

--- choices.js
+++ choices.js
@@ -1,31 +1,31 @@
 const abc123 = {
-  msg: "1Welcome. Chose your options",
+  msg: "1. Welcome. Chose your options",
   options:{
     a:{
-      msg:"2You chose A. Chose again",
-      info:"Chose option A 1",
+      msg:"2. You chose A. Choose again",
+      info:"Choose option A 1",
       options:{
         a:{
-          msg:"3Again. The end.",
-          info:"Chose option A 2",
+          msg:"3. Again. The end.",
+          info:"Choose option A 2",
         },
         b:{
           msg:"3The end.",
-          info:"Chose option B 2",
+          info:"Choose option B 2",
         }
       }
     },
     b:{
-      msg:"2you chose B. Chose again",
-      info:"Chose option B 1",
+      msg:"2. you chose B. Choose again",
+      info:"Choose option B 1",
       options:{
         a:{
-          msg:"3aaThe end.",
-          info:"Chose option A 2",
+          msg:"3. The end.",
+          info:"Choose option A 2",
         },
         b:{
-          msg:"3Again!, The end.",
-          info:"Chose option B 3",
+          msg:"3. Again!, The end.",
+          info:"Choose option B 3",
         }
       }
     }
